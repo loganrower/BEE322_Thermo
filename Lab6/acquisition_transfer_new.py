672b6951f938c965e0731f8c24ca86f7c7ac9e3f
lkrower <lkrower2000@gmail.com>
2022-05-05 10:07:08 -0700

    New Update to Run from Terminal

--- Lab6/acquisition_transfer_new.py
+++ Lab6/acquisition_transfer_new.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import csv
 import numpy as np
 import time 
-ser = serial.Serial("COM3", baudrate = 115200, timeout=1)
+from curses import baudrate
+ser = serial.Serial("COM3", 9600, timeout=1)
 
 while 1:
     arduinoData=ser.readline()
